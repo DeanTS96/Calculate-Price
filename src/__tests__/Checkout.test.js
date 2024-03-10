be7983eac6844626bc1f13557ac18acb44999cef
Dean <dean96@live.co.uk>
2024-03-10 19:36:14 +0000

    added further checks to first test to check the correct pricing property is constructed

--- src/__tests__/Checkout.test.js
+++ src/__tests__/Checkout.test.js
@@ -1,11 +1,12 @@
 import Checkout from '../utils'
 
 describe('Checkout', () => {
-    test('Checkout class has two properties, checkoutList and pricing', () => {
+    test('Checkout class has the correct properties and methods, and constructs the correct pricing property', () => {
         const testCheckout = new Checkout({a:{price: 30, specialPrice:{quantity:2, price: 50}}});
+        const expected = {a:{price: 30, specialPrice:{quantity:2, price: 50}}}
 
         expect(testCheckout).toHaveProperty('checkoutList');
-        expect(testCheckout).toHaveProperty('pricing');
+        expect(testCheckout.pricing).toEqual(expected);
         expect(testCheckout).toHaveProperty('scan');
         expect(testCheckout).toHaveProperty('getTotalPrice');
     })
